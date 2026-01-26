a39b145367a22d7698757f262196049affac42e5
pc-dev-prg <petr@cafourek.online>
2026-01-26 12:04:36 +0100

    SEO Enhancements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [svelte()],
+  site: 'https://glassnotes.app',
+  integrations: [svelte(), sitemap()],
 });
